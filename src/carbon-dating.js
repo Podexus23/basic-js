0926fa9bebb4f61ecb2027638f8fdc6fa7d62053
Podexus23 <santon23@gmail.com>
2021-01-15 02:09:55 +0300

    carbon dating is broken, dream-team-solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const HALF_LIFE_PERIOD= 5730; //k
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+// module.exports = function dateSample(str) {
+//   if(typeof str =='string' && typeof +str == 'number' && +str > 0 && Math.ceil(+str) <= 15){
+//     let time = Math.log(MODERN_ACTIVITY/+str)/0.0001208978
+//     return time
+//   }
+//   return false
+// };
+
+module.exports = function dateSample(str) {return false}
